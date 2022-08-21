2828e430ccdacd83687eb1143cbac73935ffca1a
YehosuaEs <yehosuaes@gmail.com>
2022-08-21 20:21:55 +0200

    adding styles to signup/sigin and the errors logical to signup

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -1,51 +1,66 @@
 <template>
-    <div>Sign In</div>
-    <div>
-        Don’t have an account? &nbsp;
-        <PersonalRouter :route="route" :buttonText="buttonText" />
-    </div>
-    <form @submit.prevent="handleSignIn">
-        <div>
-            <p>Fill in this form to login into your account.</p>
-            <hr />
-
-            <div>
-                <label for="email"><b>Email</b></label>
+    <section>
+        <div class="container">
+            <div class="titleContainer">
+                <h1>Listito</h1>
+                <p>Your to-do list App</p>
             </div>
-            <div>
-                <input
-                    type="text"
-                    name="email"
-                    id="email"
-                    v-model="email"
-                    placeholder="Enter your Email"
-                />
+            <div class="subtitleContainer">
+                <h4>Hello again!</h4>
+                <h4>Welcome back</h4>
             </div>
 
-            <div>
-                <label for="password"><b>Password</b></label>
-            </div>
-            <div>
-                <input
-                    :type="passwordInputType"
-                    name="password"
-                    id="password"
-                    v-model="password"
-                    placeholder="Enter your Password"
+            <form @submit.prevent="handleSignIn">
+                <div class="formContainer">
+                    <div>
+                        <label for="email"><p>Email</p></label>
+                    </div>
+                    <div>
+                        <input
+                            type="text"
+                            name="email"
+                            id="email"
+                            v-model="email"
+                            placeholder="Enter your Email"
+                            class="input"
+                        />
+                    </div>
+
+                    <div>
+                        <label for="password"><p>Password</p></label>
+                    </div>
+                    <div class="passwordInput">
+                        <input
+                            :type="passwordInputType"
+                            name="password"
+                            id="password"
+                            v-model="password"
+                            placeholder="Enter your Password"
+                            class="input"
+                        />
+                        <i
+                            :class="passwordIconClick"
+                            @click="hidePassword = !hidePassword"
+                            class="material-icons iconEye"
+                        >
+                            {{ icon }}
+                        </i>
+                    </div>
+                    <div>
+                        <button type="submit">Log in</button>
+                    </div>
+                </div>
+            </form>
+            <div class="haveSign">
+                Don’t have an account? &nbsp;
+                <PersonalRouter
+                    :route="route"
+                    :buttonText="buttonText"
+                    class="haveSign_text"
                 />
-                <i
-                    :class="passwordIconClick"
-                    @click="hidePassword = !hidePassword"
-                    class="material-icons iconEye"
-                >
-                    {{ icon }}
-                </i>
-            </div>
-            <div>
-                <button type="submit">Log in</button>
             </div>
         </div>
-    </form>
+    </section>
 </template>
 
 <script setup>
@@ -94,7 +109,8 @@ const handleSignIn = async () => {
         // calls the user store and send the users info to backend to logIn
         await user.signIn(email.value, password.value);
         // redirects user to the homeView
-        redirect.push({ path: "/" });
+        // redirect.push({ path: "/" });
+        redirect.push({ path: "/auth/login" });
     } catch (error) {
         // displays error message
         errorMsg.value = `Error: ${error.message}`;
@@ -107,26 +123,81 @@ const handleSignIn = async () => {
 </script>
 
 <style scoped>
+section {
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.container {
+    /* border: 1px solid red; */
+    padding: 0rem 0rem;
+    width: 280px;
+}
+.titleContainer {
+    text-align: center;
+    margin-bottom: 80px;
+}
+
+.subtitleContainer {
+    margin-bottom: 26px;
+}
+
+.input {
+    width: 100%;
+    margin-bottom: 1.25rem;
+    padding: 1rem 0.5rem;
+    border: 1px solid #dadee2;
+    border-radius: 10px;
+    box-shadow: 0px 3px 15px -4px #dadee2, 0px 2px 10px -4px #dadee2;
+    letter-spacing: 0.5px;
+}
+
+.errorInput {
+    border: 1.5px solid #f24452;
+    box-shadow: 0px 2px 13px -10px #f24452;
+}
+
+input:focus {
+    outline: none;
+    border: 1.5px solid #034c8c;
+}
+.passwordInput {
+    position: relative;
+}
 .iconEye {
     cursor: pointer;
+    color: #707070;
+    position: absolute;
+    top: 15px;
+    right: 20px;
 }
-form {
-    display: flex;
-    flex-direction: column;
-    margin: 1rem 0;
-}
-input {
-    color: black;
-    margin-bottom: 1rem;
-}
+
 button {
-    background-color: #4caf50; /* Green */
-    border: none;
+    background: #034c8c;
+    border: 2px solid #034c8c;
+    color: #f6f7f8;
+    border-radius: 50px;
     color: white;
-    padding: 10px 10px;
+    margin: 1rem 0;
+    padding: 1rem 1.25rem;
     text-align: center;
     text-decoration: none;
-    display: inline-block;
     font-size: 16px;
+    cursor: pointer;
+    width: 100%;
+}
+button:hover {
+    background-color: #fff;
+    border: 2px solid #034c8c;
+    color: #034c8c;
+}
+
+.haveSign {
+    margin: 15px 0;
+    text-align: center;
+}
+.haveSign_text {
+    color: #034c8c;
 }
 </style>
--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -3,18 +3,20 @@
         <div class="container">
             <div class="titleContainer">
                 <h1>Listito</h1>
-                <p>Your todo list app</p>
+                <p>Your to-do list App</p>
             </div>
             <div class="subtitleContainer">
-                <h3>Welcome <br /></h3>
-                <p>Signup to get start</p>
+                <h4>Hello!</h4>
+                <h4>Signup to get started</h4>
             </div>
             <!-- <div > -->
             <form @submit.prevent="handleSignUp">
                 <div class="formContainer">
                     <!-- ------- email ------- -->
                     <div>
-                        <label for="email"><b>Email</b></label>
+                        <label for="email"
+                            ><p><!-- Email --></p></label
+                        >
                     </div>
                     <div>
                         <input
@@ -23,19 +25,33 @@
                             id="email"
                             v-model="email"
                             placeholder="Enter your Email"
+                            class="input"
+                            :class="
+                                errorValue || errorCheckEmail
+                                    ? 'errorInput'
+                                    : 'input'
+                            "
                         />
                     </div>
                     <!-- ------- password ------- -->
                     <div>
-                        <label for="password"><b>Password</b></label>
+                        <label for="password"
+                            ><p><!-- Password --></p></label
+                        >
                     </div>
-                    <div>
+                    <div class="passwordInput">
                         <input
                             :type="passwordInputType"
                             name="password"
                             id="password"
                             v-model="password"
                             placeholder="Enter your Password"
+                            class="input"
+                            :class="
+                                errorPasswordSame || errorCheckPassword
+                                    ? 'errorInput'
+                                    : 'input'
+                            "
                         />
                         <i
                             :class="passwordIconClick"
@@ -49,7 +65,7 @@
 
                     <div>
                         <label for="passwordRepeat"
-                            ><b>Repeat Password</b></label
+                            ><p><!-- Repeat Password --></p></label
                         >
                     </div>
                     <div>
@@ -59,17 +75,27 @@
                             id="repeatPassword"
                             v-model="repeatPassword"
                             placeholder="Confirm your Password"
+                            class="input"
+                            :class="
+                                errorPasswordSame || errorCheckPassword
+                                    ? 'errorInput'
+                                    : 'input'
+                            "
                         />
                     </div>
                     <!-- ------- button submit ------- -->
                     <div>
-                        <button type="submit">Sign up</button>
+                        <button type="submit">SIGN UP</button>
                     </div>
                 </div>
             </form>
             <div class="haveSign">
-                Have an account? &nbsp;
-                <PersonalRouter :route="route" :buttonText="buttonText" />
+                Already have an account?
+                <PersonalRouter
+                    :route="route"
+                    :buttonText="buttonText"
+                    class="haveSign_text"
+                />
             </div>
             <!-- </div> -->
         </div>
@@ -94,12 +120,17 @@ const redirect = useRouter();
 const email = ref("");
 const password = ref("");
 const repeatPassword = ref("");
+
 // Icons Show and hide password & const to manage
 const hidePassword = ref(true);
 const icon = ref("visibility_off");
 // Error Message
 const errorMsg = ref("");
-// To confirm password
+// Error Input
+const errorNoValue = ref(false);
+const errorCheckEmail = ref(false);
+const errorPasswordSame = ref(false);
+const errorCheckPassword = ref(false);
 
 /* -----------------------METHODS and FUNCTIONS----------------------- */
 // To verify correct email
@@ -128,18 +159,26 @@ const passwordInputType = computed(() =>
 const handleSignUp = async () => {
     try {
         if (!email.value || !password.value) {
-            alert("missing infoo");
+            alert("missing info");
+            errorNoValue.value = true;
         } else if (!checkEmail(email.value)) {
             alert("Valid email required.");
+            errorNoValue.value = false;
+            errorCheckEmail.value = true;
         } else if (password.value !== repeatPassword.value) {
             alert("the password doesn't match");
+            errorCheckEmail.value = false;
+            errorPasswordSame.value = true;
         } else if (!checkPassword(password.value)) {
             alert(
                 "Passwords must contain at least six characters, including uppercase, lowercase, numbers and a special character"
             );
+            errorPasswordSame.value = false;
+            errorCheckPassword.value = true;
         } else {
             // calls the user from store and send the users info to backend to login
             await user.signUp(email.value, password.value);
+            errorCheckPassword.value = false;
             console.log(email.value + " si entraste  con tu email well done");
             // redirect.push({ path: "/auth/login" });
             redirect.push({ path: "/auth/sign-up" });
@@ -151,6 +190,7 @@ const handleSignUp = async () => {
         setTimeout(() => {
             errorMsg.value = null;
         }, 5000);
+    } finally {
     }
 };
 </script>
@@ -163,9 +203,9 @@ section {
     align-items: center;
 }
 .container {
-    border: 1px solid red;
-    width: 100%;
-    padding: 0rem 2rem;
+    /* border: 1px solid red; */
+    padding: 0rem 0rem;
+    width: 280px;
 }
 .titleContainer {
     text-align: center;
@@ -173,39 +213,67 @@ section {
 }
 
 .subtitleContainer {
-    color: green;
-    margin-bottom: 20px;
+    margin-bottom: 26px;
 }
-.containerForm {
-    /* margin-top: 60px; */
+
+.formContainer {
+}
+
+.input {
+    width: 100%;
+    margin-bottom: 1.25rem;
+    padding: 1rem 0.5rem;
+    border: 1px solid #dadee2;
+    border-radius: 10px;
+    box-shadow: 0px 3px 15px -4px #dadee2, 0px 2px 10px -4px #dadee2;
+    letter-spacing: 0.5px;
+}
+
+.errorInput {
+    border: 1.5px solid #f24452;
+    box-shadow: 0px 2px 13px -10px #f24452;
+}
+
+input:focus {
+    outline: none;
+    border: 1.5px solid #f2a74b;
+}
+.passwordInput {
+    position: relative;
 }
 .iconEye {
     cursor: pointer;
+    color: #707070;
+    position: absolute;
+    top: 15px;
+    right: 20px;
 }
 
-form {
-    /* display: flex;
-    flex-direction: column;
-    margin: 1rem 0;
-    justify-content: center;
-    align-items: center; */
-}
-input {
-    margin-bottom: 1rem;
-    width: 100%;
-}
 button {
-    background-color: #4caf50;
-    border: none;
+    background: #f2a74b;
+    border: 2px solid #f2a74b;
+    color: #f6f7f8;
+    border-radius: 50px;
     color: white;
-    padding: 10px 10px;
+    margin: 1rem 0;
+    padding: 1rem 1.25rem;
     text-align: center;
     text-decoration: none;
     font-size: 16px;
     cursor: pointer;
+    width: 100%;
+}
+button:hover {
+    background-color: #fff;
+    border: 2px solid #f2a74b;
+    color: #f2a74b;
 }
 
 .haveSign {
-    margin-bottom: 30px;
+    margin: 15px 0;
+    text-align: center;
+}
+.haveSign_text {
+    color: #f2a74b;
 }
 </style>
